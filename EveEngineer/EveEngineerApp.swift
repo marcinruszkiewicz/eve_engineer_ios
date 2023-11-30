a7a38f2a7a2339e24b08af480a152ee3e05b972e
Marcin Ruszkiewicz <marcin.ruszkiewicz@polcode.net>
2023-11-30 13:08:05 +0100

    Initial Commit

---
+++ EveEngineer/EveEngineerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EveEngineerApp.swift
+//  EveEngineer
+//
+//  Created by Marcin Ruszkiewicz on 30/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct EveEngineerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
